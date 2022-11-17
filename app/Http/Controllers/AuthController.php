09450d036a858606ec59bd1b66e28352b7ef0827
Ford1508 <phuclc1508@gmail.com>
2022-11-17 17:34:51 +0700

    add userSeeder, add UserResource

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
@@ -19,26 +23,38 @@ class AuthController extends Controller
         $user->email = $request->email;
         $user->password = Hash::make($request->password);
         $user->username = $request->username;
-        $user->gender = $request->gender;
-        $user->birth = $request->birth;
-        $user->save();
-        return response()->json($user, 200);
+        ($request->gender) ? $user->gender = $request->gender: "";
+        ($request->birth) ? ($user->birth = $request->birth): "";
+        try {
+            $user->save();
+            $user = User::find($user->id);
+            return response()->json(new UserResource($user), 200);
+        } catch (\Exception $e) {
+            return response()->json(["error"=>"Email or Username is already exists!"], 500);
+        }
     }
     
     public function login(Request $request){
         if(filter_var($request->email,FILTER_VALIDATE_EMAIL)){
             $user = User::where('email', $request->email)->first();
+            if($request->email == "" || $request->password == ""){
+                return response()->json(["error"=>"Email or password is empty!"], 404);
+            }
+            if(!$user || !Hash::check($request->password, $user->password)){
+                return response()->json(["error"=>"Email or password is not matched"], 404);
+            }
+            return response()->json(new UserResource($user), 200);
         }
         else{
-            $user = User::where('username', $request->email)->first();
+            $user = User::where('username', $request->username)->first();
+            if($request->username == "" || $request->password == ""){
+                return response()->json(["error"=>"Username or password is empty!"], 404);
+            }
+            if(!$user || !Hash::check($request->password, $user->password)){
+                return response()->json(["error"=>"Username or password is not matched"], 404);
+            }
+            return response()->json(new UserResource($user), 200);
         }
-        if($request->email == "" || $request->password == ""){
-            return response()->json(["error"=>"Email or password is empty!"], 404);
-        }
-        if(!$user || !Hash::check($request->password, $user->password)){
-            return response()->json(["error"=>"Email or password is not matched"], 404);
-        }
-        return response()->json($user, 200);
     }
 
 
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'Uchiha Madara',
+                'username' => 'admin',
+                'email' => 'admin@gmail.com',
+                'gender' => 0,
+                'user_type' => 2,
+                'password' => Hash::make('123456')
+            ],
+            [
+                'name' => 'Uchiha Itachi',
+                'username' => 'poster',
+                'email' => 'poster@gmail.com',
+                'gender' => 0,
+                'user_type' => 1,
+                'password' => Hash::make('123456')
+            ],
+            [
+                'name' => 'Uchiha Sasuke',
+                'username' => 'user01',
+                'email' => 'user01@gmail.com',
+                'gender' => 0,
+                'user_type' => 0,
+                'password' => Hash::make('123456')
+            ],
+        ]);
+    }
+}
