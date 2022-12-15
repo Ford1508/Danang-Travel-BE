4b4e908d52bc27ccdb0728728d5d366e4c0b13a9
Ford1508 <phuclc1508@gmail.com>
2022-12-16 03:49:46 +0700

    deploy stage

--- routes/api.php
+++ routes/api.php
@@ -53,7 +53,6 @@ Route::get('/blog/unrate/{id}', [RatingPostController::class, 'unrateblog']);
 Route::post('/comment/add/{id}', [CommentController::class, 'blogcomment']);
 Route::delete('/comment/delete/{id}', [CommentController::class, 'deletecomment']);
 Route::get('/comment/byblog/{id}', [CommentController::class, 'comments_by_blog']);
-// Route::get('/comment/add', [CommentController::class, 'blogcomment']);
 
 Route::post('/comment/rating/{id}', [RatingCommentController::class, 'ratecomment']);
 Route::post('/comment/unrate/{id}', [RatingCommentController::class, 'unratecomment']);
