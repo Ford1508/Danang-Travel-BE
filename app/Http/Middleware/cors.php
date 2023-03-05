4531556e2384f06d6e3981b0bd84f7b973c8d331
Ford1508 <phuclc1508@gmail.com>
2023-03-05 23:04:01 +0700

    config cors

--- app/Http/Middleware/cors.php
+++ app/Http/Middleware/cors.php
@@ -19,7 +19,6 @@ class cors
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', '*')
-            ->header('Access-Control-Allow-Credentials', 'true')
-            ->header('Access-Control-Allow-Headers', '*');
+            ->header('Access-Control-Allow-Headers', 'Content-type, X-Auth-Token, Authorization, Origin');
     }
 }
